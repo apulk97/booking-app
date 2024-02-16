e39f6de090a6e14cdb21f428090a6f5a5f057238
apulk97 <parihar.adityapulkit@gmail.com>
2024-02-16 22:26:15 +0530

    Added sign in ui changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,13 @@
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store"; 
 import { Link } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import { signout } from "../slices/authSlice";
 
 const Header = () => {
-  const isLoggedIn = localStorage.getItem("profile");
+  const {authData} = useSelector((state: RootState) => state.auth)
+  const isLoggedIn = localStorage.getItem("profile")
+  const dispatch: AppDispatch = useDispatch()
   return (
     <div className="bg-blue-800 py-6">
       <ToastContainer />
@@ -15,7 +20,7 @@ const Header = () => {
             <span className="text-white font-bold text-xl"><Link to="/my-bookings">My Bookings</Link></span>
             <span className="text-white font-bold text-xl"><Link to="/my-hotels">My Hotels</Link></span>
 
-            <button className="bg-white text-blue-600 font-bold px-3" onClick={() => {localStorage.clear(); toast.success('Signed out successfully')}}>
+            <button className="bg-white text-blue-600 font-bold px-3" onClick={() => dispatch(signout())}>
               <Link to="/">Sign Out</Link>
             </button>
           </nav>
