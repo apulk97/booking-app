34f3e4b0dc0f7af806a053f8c5472841e94092c1
iamshk1709 <shikharkumar.hbtu@gmail.com>
2024-02-20 14:09:36 +0530

    style changes

--- frontend/src/pages/Search/index.tsx
+++ frontend/src/pages/Search/index.tsx
@@ -16,6 +16,8 @@ function Search() {
   const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
   const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
   const [selectedPrice, setSelectedPrice] = useState<number | undefined>();
+  const [flexValueFirst, setFlexValueFirst] = useState(1);
+  const [flexValueSecond, setFlexValueSecond] = useState(4);
 
   useEffect(() => {
     const fetchSearch = async () => {
@@ -56,7 +58,7 @@ function Search() {
         : prevStars.filter((star) => star !== starRating)
     );
   };
-  
+
   const handleHotelTypeChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -80,9 +82,9 @@ function Search() {
   };
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-      <div className="rounded-lg border border-slate-300 p-5 h-fit top-10 ">
-        <div className="space-y-5">
+    <div className="flex flex-row justify-between flex-1">
+      <div style={{ margin: 5, flex: window.innerWidth < 1024 ? 1 : 1 }}>
+        <div className="space-y-5 rounded-lg border border-slate-300 p-5 h-fit top-10 sticky ">
           <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
             Filter by:
           </h3>
@@ -104,7 +106,7 @@ function Search() {
           />
         </div>
       </div>
-      <div>
+      <div style={{ margin: 5, flex: window.innerWidth < 1024 ? 2 : 4 }}>
         <div className="flex flex-col gap-4">
           {loading && <div>Loading...</div>}
           {searchData?.data?.length === 0 ? (
